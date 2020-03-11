67af956cbcbad33767bb63b268cc1465e863e465
Ramy Carter <tbone425@msn.com>
2020-03-10 18:49:46 -0700

    change file name

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
+//user facing
+
+$(document).ready(function () {
+    $("form#numbers").submit(function (event) {
+        event.preventDefault();
+        var inputValue = parseInt($("form#numbers input").val());
+        var response = $("#response");
+        var startarray = [];
+        var finalarray = [];
+        for (var i = 0; i <= inputValue; i++) {
+            i = i.toString();
+            startarray.push(i);
+        }
+});
+})
+
 //backend
 
-//user facing
-$(document).ready(function() {
-    $("form#numbers").submit(function(event) {
-        event.preventDefault();
-        var output = parseInt($("form#numbers input").val());
-    });
-})
+for (var i = 0; i < startarray.length; i++){
+    if (startarray[i].includes("3")){
+        finalarray.push("Won't you be my neighbor?");
+    } else if (startarray[i].includes("2")){
+        finalarray.push("Boop!");
+    } else if (startarray[i].includes("1")){
+        finalarry.push("Beep!");
+    } else {
+        finalarray.push(i);
+    }
+}
