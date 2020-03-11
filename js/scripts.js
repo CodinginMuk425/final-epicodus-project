73bbe6c1920b86ea454ea0eb9d867a421e40dd78
Ramy Carter <tbone425@msn.com>
2020-03-10 21:34:54 -0700

    add logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
+//backend
+var startarray = [];
+var finalarray = [];
+//var outcome = $("#result");
+//console.log(outcome.text());
+function finalresult() {
+    for (var i = 0; i < startarray.length; i++) {
+        if (startarray[i].includes("3")) {
+            finalarray.push("Won't you be my neighbor?");
+        } else if (startarray[i].includes("2")) {
+            finalarray.push("Boop!");
+        } else if (startarray[i].includes("1")) {
+            finalarray.push("Beep!");
+        } else {
+            finalarray.push(i);
+        }
+    }
+    $("#result").text(finalarray).show();    
+}
+
+
 //user facing
 
 $(document).ready(function () {
     $("form#numbers").submit(function (event) {
         event.preventDefault();
         var inputValue = parseInt($("form#numbers input").val());
-        var response = $("#response");
-        var startarray = [];
-        var finalarray = [];
         for (var i = 0; i <= inputValue; i++) {
             i = i.toString();
             startarray.push(i);
-        }
-});
+        } 
+        console.log(inputValue);
+        finalresult();
+    });
 })
-
-//backend
-
-for (var i = 0; i < startarray.length; i++){
-    if (startarray[i].includes("3")){
-        finalarray.push("Won't you be my neighbor?");
-    } else if (startarray[i].includes("2")){
-        finalarray.push("Boop!");
-    } else if (startarray[i].includes("1")){
-        finalarry.push("Beep!");
-    } else {
-        finalarray.push(i);
-    }
-}
